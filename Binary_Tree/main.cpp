3c41e807ad2788173878e676ac38915f7060ea11
Neutral <neutralobs@gmail.com>
2025-01-18 18:05:19 +0330

    Comments added

--- Binary_Tree/main.cpp
+++ Binary_Tree/main.cpp
@@ -16,6 +16,7 @@ private:
     Node<T> *head = nullptr;
     int height = 0;
 
+    // Private function that will be used to delete the tree
     void delete_tree(Node<T>* node) {
         if (node) {
             delete_tree(node->left);
@@ -24,6 +25,7 @@ private:
         }
     }
 
+    // Private function that is used to add a branch in case of an existing head
     bool add_recursive(T data, Node<T> *node, int current_height)
     {
         if(node->left == nullptr)
@@ -51,6 +53,7 @@ private:
 public:
     ~Binary_Tree() { delete_tree(head); }
 
+    // Creates the first node in case of an empty head. Otherwise calls the private add function
     bool add(T data)
     {
         if(head == nullptr)
